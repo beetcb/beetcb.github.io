3147fb639a8875006813b7c25d77df25a167a271
beet-source-tree <zpharnoex@gmail.com>
2020-06-28 23:03:37 +0800

    working on index layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,81 +1,38 @@
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const fs = require('fs');
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const yaml = require("js-yaml");
 
-// Import filters
-const dateFilter = require('./src/filters/date-filter.js');
-const markdownFilter = require('./src/filters/markdown-filter.js');
-const w3DateFilter = require('./src/filters/w3-date-filter.js');
+module.exports = eleventyConfig => {
+    /* plugin */
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
-// Import transforms
-const htmlMinTransform = require('./src/transforms/html-min-transform.js');
-const parseTransform = require('./src/transforms/parse-transform.js');
+    /* rendless but needed copy */
+    eleventyConfig.addPassthroughCopy("src/");
 
-// Import data files
-const site = require('./src/_data/site.json');
+    /* default layout template */
+	eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+	eleventyConfig.addLayoutAlias('page', 'layouts/page.njk');
+    eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+    
+    /* use custom data as a blog config set -> Yaml */
 
-module.exports = function(config) {
-  // Filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('markdownFilter', markdownFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+    eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
 
-  // Layout aliases
-  config.addLayoutAlias('home', 'layouts/home.njk');
-
-  // Transforms
-  config.addTransform('htmlmin', htmlMinTransform);
-  config.addTransform('parse', parseTransform);
-
-  // Passthrough copy
-  config.addPassthroughCopy('src/fonts');
-  config.addPassthroughCopy('src/images');
-  config.addPassthroughCopy('src/js');
-  config.addPassthroughCopy('src/admin/config.yml');
-  config.addPassthroughCopy('src/admin/previews.js');
-  config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
-  config.addPassthroughCopy('src/robots.txt');
-
-  const now = new Date();
-
-  // Custom collections
-  const livePosts = post => post.date <= now && !post.data.draft;
-  config.addCollection('posts', collection => {
-    return [
-      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
-    ].reverse();
-  });
-
-  config.addCollection('postFeed', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
-      .reverse()
-      .slice(0, site.maxPostsPerPage);
-  });
-
-  // Plugins
-  config.addPlugin(rssPlugin);
-  config.addPlugin(syntaxHighlight);
-
-  // 404
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
-
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
-
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist'
-    },
-    passthroughFileCopy: true
-  };
-};
+    return {
+        /*
+        dir: {
+            input: "build",
+            output: "sequel",
+            includes: "templates"
+        }, 
+        custom 11ty settings
+        */
+        templateFormats: [
+            "html",
+            "md",
+            "liquid",
+            "njk"
+        ],
+        htmlTemplateEnging: "njk",
+        markdownTemplateEngine: "njk"
+    };
+} 
