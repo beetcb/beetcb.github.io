b548b7e3b97b442fc148ba7cf3a8691103cb89b9
beet <zpharnoex@gmail.com>
2020-08-16 22:41:21 +0800

    add <break> bt date and location

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -13,7 +13,7 @@ exports.render = function(data) {
 	<article class="article">
 	    <h1 class="title">${ data.title }</h1>
 	    <div class="location content">
-		<a href="/">Home</a> ${ data.page.url }
+		<a href="/">Home</a> ${ data.page.url }<br>
 		<time datetime="${ formatDate(data.date) }">${ formatDate(data.date) }</time>
 	    </div>
 	    <div class="content">
