8557347c85f0f177fd446ff57907994748e3ee00
be beet <63141491+beetcb@users.noreply.github.com>
2021-01-30 06:51:15 -0800

    fix: mini-css-extract-plugin breaking change

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,6 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
           },
           'css-loader',
         ],
